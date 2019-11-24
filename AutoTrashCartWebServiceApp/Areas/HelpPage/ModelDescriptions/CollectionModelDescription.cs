8e8ff02ae519ac98a0908779f418898ac8782799
Nagesh <nageshvmr@gmail.com>
2019-11-24 02:06:09 -0600

    Default boilerplate code

---
+++ AutoTrashCartWebServiceApp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AutoTrashCartWebServiceApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AutoTrashCartWebServiceApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AutoTrashCartWebServiceApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
