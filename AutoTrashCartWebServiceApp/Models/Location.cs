72186f55d4f921169a54255f1b0eb6ac414bed67
Nagesh <nageshvmr@gmail.com>
2019-11-25 15:22:52 -0600

    Fixed data format

--- AutoTrashCartWebServiceApp/Models/Location.cs
+++ AutoTrashCartWebServiceApp/Models/Location.cs
@@ -4,11 +4,11 @@
     {
         public string Token { get; set; }
         public string LocationType { get; set; }
-        public float Latitude0 { get; set; }
-        public float Longitude0 { get; set; }
-        public float Latitude1 { get; set; }
-        public float Longitude1 { get; set; }
-        public float Latitude2 { get; set; }
-        public float Longitude2 { get; set; }
+        public decimal Latitude0 { get; set; }
+        public decimal Longitude0 { get; set; }
+        public decimal Latitude1 { get; set; }
+        public decimal Longitude1 { get; set; }
+        public decimal Latitude2 { get; set; }
+        public decimal Longitude2 { get; set; }
     }
 }
--- AutoTrashCartWebServiceApp/Models/Orientation.cs
+++ AutoTrashCartWebServiceApp/Models/Orientation.cs
@@ -4,8 +4,8 @@
     {
         public string Token { get; set; }
         public string OrientationType { get; set; }
-        public float X { get; set; }
-        public float Y { get; set; }
-        public float Z { get; set; }
+        public decimal X { get; set; }
+        public decimal Y { get; set; }
+        public decimal Z { get; set; }
     }
 }
